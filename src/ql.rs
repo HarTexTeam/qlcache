bc6591f2fe00f86fba5e9ac0a1bf459bf723fed6
HTG-YT <htgazurex1212.0001@gmail.com>
2021-10-15 17:48:25 +0800

    fix error...?

--- src/ql.rs
+++ src/ql.rs
@@ -93,7 +93,7 @@ fn parse(query: &str) -> QlResult<Command> {
                 // check if the next lexeme is `FROM`
                 let mut peekable = segments.clone().peekable();
                 let next = peekable.peek();
-                
+
                 if next.is_none() || next.unwrap() != &"FROM" {
                     return Err(QlError::ParseQueryError(
                         ParseQueryErrorKind::SyntaxError {
@@ -132,7 +132,9 @@ fn parse(query: &str) -> QlResult<Command> {
                 else {
                     // there are still more lexemes to analyze
                     // check if the next lexeme is `WHERE` for constraints
-                    let next = segments.clone().peekable().peek();
+                    let mut peekable = segments.clone().peekable();
+                    let next = peekable.peek();
+                    
                     if next.is_none() || next.unwrap() != &"FROM" {
                         return Err(QlError::ParseQueryError(
                             ParseQueryErrorKind::SyntaxError {
