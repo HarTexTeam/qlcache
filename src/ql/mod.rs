483df65b1a8f921077741ccdd68750ac058b06fe
HTG-YT <39023054+HTG-YT@users.noreply.github.com>
2021-10-27 20:54:08 +0800

    move constraints to its own module

--- src/ql/mod.rs
+++ src/ql/mod.rs
@@ -2,18 +2,12 @@
 //!
 //! This module implements the query language.
 
-use crate::{
-    error::{
-        QlError,
-        QlResult
-    },
-    ql::select::{
-        Select,
-        SelectBuilder
-    },
-    ColumnValue
+use crate::ql::select::{
+    Select,
+    SelectBuilder
 };
 
+pub mod constraints;
 pub mod select;
 
 /// # Trait `QueryKind`
@@ -28,161 +22,17 @@ pub struct Query<T: QueryKind> {
     pub(crate) query: T
 }
 
-impl<T: QueryKind> Query<T> {
-    #[must_use]
-    pub fn builder() -> QueryBuilder {
-        QueryBuilder {}
-    }
-}
-
 /// # Struct `QueryBuilder`
 ///
 /// The builder for a `Query`.
 pub struct QueryBuilder;
 
 impl QueryBuilder {
-    /// # Instance Method `QueryBuilder::select`
+    /// # Static Method `QueryBuilder::select`
     ///
     /// Returns a builder for building a `SELECT` query.
     #[must_use]
     pub fn select() -> SelectBuilder {
         Select::builder()
     }
-}
-
-/// # Struct `Constraint`
-///
-/// A constraint for data queries.
-#[derive(Clone, Eq, PartialEq)]
-pub struct Constraint {
-    pub(crate) field_name: String,
-    pub(crate) op: ConstraintOp,
-    pub(crate) value: ColumnValue
-}
-
-impl Constraint {
-    /// # Instance Method `Constraint::builder`
-    ///
-    /// Returns a builder for a `Constraint`
-    #[must_use]
-    pub fn builder() -> ConstraintBuilder {
-        ConstraintBuilder {
-            field_name: None,
-            op: None,
-            value: None
-        }
-    }
-}
-
-/// # Struct `ConstraintBuilder`
-///
-/// A builder for a `Constraint`.
-#[derive(Clone, Eq, PartialEq)]
-pub struct ConstraintBuilder {
-    pub(crate) field_name: Option<String>,
-    pub(crate) op: Option<ConstraintOp>,
-    pub(crate) value: Option<ColumnValue>
-}
-
-impl ConstraintBuilder {
-    /// # Instance Method `ConstraintBuilder::field_name`
-    ///
-    /// Sets the field name for the constraint.
-    ///
-    /// ## Parameters
-    /// - `field_name`, type `String`; the field name to set
-    #[must_use]
-    pub fn field_name(mut self, field_name: String) -> Self {
-        self.field_name.replace(field_name);
-        self
-    }
-
-    /// # Instance Method `ConstraintBuilder::op`
-    ///
-    /// Sets the "constraint relational operator" for the constraint.
-    ///
-    /// ## Parameters
-    /// - `op`, type `ConstraintOp`; the "constraint relational operator" name to set
-    #[must_use]
-    pub fn op(mut self, op: ConstraintOp) -> Self {
-        self.op.replace(op);
-        self
-    }
-
-    /// # Instance Method `ConstraintBuilder::value`
-    ///
-    /// Sets the value to check against for the constraint.
-    ///
-    /// ## Parameters
-    /// - `op`, type `ConstraintOp`; the "constraint relational operator" name to set
-    #[must_use]
-    pub fn value(mut self, value: ColumnValue) -> Self {
-        self.value.replace(value);
-        self
-    }
-
-    /// # Instance Method `ConstraintBuilder::build`
-    ///
-    /// Consumes the builder and returns a `Constraint`.
-    ///
-    /// ## Errors
-    ///
-    /// Returns `RequiredFieldIsNone` if any of the fields required is `None`.
-    #[allow(clippy::missing_panics_doc)] // this function would never panic
-    pub fn build(self) -> QlResult<Constraint> {
-        if self.field_name.is_none() {
-            return Err(QlError::RequiredFieldIsNone {
-                field_name: String::from("Constraint.field_name")
-            });
-        }
-
-        if self.op.is_none() {
-            return Err(QlError::RequiredFieldIsNone {
-                field_name: String::from("Constraint.op")
-            });
-        }
-
-        if self.value.is_none() {
-            return Err(QlError::RequiredFieldIsNone {
-                field_name: String::from("Constraint.value")
-            });
-        }
-
-        Ok(Constraint {
-            field_name: self.field_name.unwrap(),
-            op: self.op.unwrap(),
-            value: self.value.unwrap()
-        })
-    }
-}
-
-/// # Enumeration `ConstraintOp`
-///
-/// Represents a "constraint relational operator".
-#[derive(Clone, Eq, PartialEq)]
-pub enum ConstraintOp {
-    /// # Enumeration Variant `ConstraintOp::Eq`
-    ///
-    /// `=`
-    Eq,
-
-    /// # Enumeration Variant `ConstraintOp::Lt`
-    ///
-    /// `<`
-    Lt,
-
-    /// # Enumeration Variant `ConstraintOp::Gt`
-    ///
-    /// `>`
-    Gt,
-
-    /// # Enumeration Variant `ConstraintOp::Le`
-    ///
-    /// `<=`
-    Le,
-
-    /// # Enumeration Variant `ConstraintOp::Ge`
-    ///
-    /// `>=`
-    Ge
 }
--- src/ql/select.rs
+++ src/ql/select.rs
@@ -14,6 +14,9 @@ impl Select {
     }
 }
 
+/// # Struct `SelectBuilder`
+///
+/// A builder for a `Select`.
 #[allow(clippy::module_name_repetitions)]
 pub struct SelectBuilder {
     pub(crate) table_name: Option<String>,
@@ -21,9 +24,27 @@ pub struct SelectBuilder {
 }
 
 impl SelectBuilder {
+    /// # Instance Method `SelectBuilder::field_name`
+    ///
+    /// Sets the table name for the selection.
+    ///
+    /// ## Parameters
+    /// - `table_name`, type `String`; the table name to set
     #[must_use]
     pub fn table_name(mut self, table_name: String) -> Self {
         self.table_name.replace(table_name);
+        self
+    }
+
+    /// # Instance Method `SelectBuilder::field_name`
+    ///
+    /// Sets the selection scope for the selection.
+    ///
+    /// ## Parameters
+    /// - `scope`, type `SelectScope`; the selection scope to set
+    #[must_use]
+    pub fn scope(mut self, scope: SelectScope) -> Self {
+        self.scope.replace(scope);
         self
     }
 }
