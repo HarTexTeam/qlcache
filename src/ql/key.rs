140f24f1b8987ab1b5a46ebff21e27a5c2e0c8be
HTG-YT <39023054+HTG-YT@users.noreply.github.com>
2021-11-03 11:57:17 +0800

    remove unused imports

--- src/ql/key.rs
+++ src/ql/key.rs
@@ -2,14 +2,6 @@
 //!
 //! This module implements `PRIMARY KEY` and `FOREIGN KEY`.
 
-use crate::{
-    error::{
-        QlError,
-        QlResult
-    },
-    ColumnDataType
-};
-
 /// # Struct `PrimaryKey`
 ///
 /// The primary key of a table.
