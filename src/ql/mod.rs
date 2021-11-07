65bcf6b9f20e44be1077ee68e39b98afdbb5860f
HTG-YT <39023054+HTG-YT@users.noreply.github.com>
2021-11-07 13:34:27 +0800

    fix lifetimes

--- src/ql/mod.rs
+++ src/ql/mod.rs
@@ -30,7 +30,7 @@ pub trait QueryKind {
     /// # Trait Associated Type `ResultType`
     ///
     /// The type of the returned result from this query.
-    type ResultType<'row>;
+    type ResultType;
 
     /// # Trait Method `QueryKind::execute`
     ///
@@ -39,7 +39,7 @@ pub trait QueryKind {
     /// ## Errors
     ///
     /// Returns query-related errors.
-    fn execute(self, cache: &QlCache) -> QlResult<Self::ResultType<'_>>;
+    fn execute(self, cache: &QlCache) -> QlResult<Self::ResultType>;
 }
 
 /// # Struct `Query`
@@ -61,7 +61,7 @@ impl<T: QueryKind> Query<T> {
     /// ## Errors
     ///
     /// Returns query-related errors.
-    pub fn execute(self, cache: &QlCache) -> QlResult<T::ResultType<'_>> {
+    pub fn execute(self, cache: &QlCache) -> QlResult<T::ResultType> {
         self.query.execute(cache)
     }
 }
